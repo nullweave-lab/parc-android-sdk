591fc1ab61edfb078ac31b57311d2e7d38443e74
Axymorrsen <Axymosen@outlook.com>
2026-08-03 17:14:52 +0800

    fix(android): migrate to Kotlin compilerOptions DSL

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -17,12 +19,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     testOptions {
         unitTests.isReturnDefaultValues = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
