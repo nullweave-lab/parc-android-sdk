00fc837249a95fde189f0fc3b2e5f5dc021afe68
Axymorrsen <Axymosen@outlook.com>
2026-08-04 11:48:47 +0800

    test(android): configure framework instrumentation runner

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -15,6 +15,7 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
+        testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
 
     compileOptions {
